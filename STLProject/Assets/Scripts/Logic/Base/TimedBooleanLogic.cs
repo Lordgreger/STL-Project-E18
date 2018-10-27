ef5c680fd6eff59bdb3733fe6cd4a515c891d5af
Greger <blacknedpsi@gmail.com>
2018-10-27 02:01:17 +0200

    Added Timed pressure plate and base logic for timed logic

---
+++ STLProject/Assets/Scripts/Logic/Base/TimedBooleanLogic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedBooleanLogic : BooleanLogic {
+
+
+    public bool stableState;
+    public float relapseTime;
+
+    //[HideInInspector]
+    public float timer;
+    //[HideInInspector]
+    public bool timerPaused;
+
+    private void Start() {
+        state = stableState;
+    }
+
+    private void Update() {
+        if (!timerPaused) {
+            if (!stableState == state) {
+                if (timer != 0.0f) {
+                    timer -= Time.deltaTime;
+                    if (timer < 0.0f) {
+                        setState(stableState);
+                        onTimerDone();
+                    }
+                }
+            }
+        }
+    }
+
+    public override void setState(bool i) {
+        if (state == stableState) {
+            if (i == state) { // From stable to stable
+                // Do nothing
+            }
+            else { // From stable to unstable
+                state = !stableState;
+                timer = relapseTime;
+                onChange.Invoke();
+            }
+        }
+        else {
+            if (i == state) { // From unstable to unstable
+                timer = relapseTime; // reset timer
+            }
+            else { // From unstable to stable
+                state = stableState;
+                timer = 0.0f;
+                onChange.Invoke();
+            }
+        }
+    }
+
+    protected virtual void onTimerDone() {
+
+    }
+}
---
+++ STLProject/Assets/Scripts/Logic/TimedPressurePlate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPressurePlate : TimedBooleanLogic {
+
+    public Sprite onSprite;
+    public Sprite offSprite;
+
+    public SpriteRenderer rend;
+
+    void updateVisuals(bool state) {
+        if (state) {
+            rend.sprite = onSprite;
+        }
+        else {
+            rend.sprite = offSprite;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        print("Entered collider!");
+        if (col.gameObject.tag == "Player") { // Player entered collider
+            setState(!stableState);
+            updateVisuals(state);
+            timerPaused = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col) {
+        print("Exited collider!");
+        if (col.gameObject.tag == "Player") { // Player exited collider
+            timerPaused = false;
+        }
+    }
+
+    protected override void onTimerDone() {
+        updateVisuals(state);
+    }
+
+}
