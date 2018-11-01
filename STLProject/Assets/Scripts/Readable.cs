5a711a56d94584f1d87445cc269f70cb36c5c598
Unknown <mette.tanggaard.ohlsen@gmail.com>
2018-11-01 23:48:39 +0100

    Auto stash before rebase of "origin/master"
    scene 2 og lever sprites

---
+++ STLProject/Assets/Scripts/Readable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Readable : MonoBehaviour {
+    public GameObject hintCanvas;
+
+
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            hintCanvas.SetActive(false);
+        }
+    }
+
+    public void toggleText()
+    {
+        hintCanvas.SetActive(!hintCanvas.activeSelf);
+    }
+}
