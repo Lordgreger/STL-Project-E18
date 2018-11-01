d53e1a09258a64da3c4cebeba4e6de0155a312f9
Greger <blacknedpsi@gmail.com>
2018-11-01 20:41:41 +0100

    SomeStuff

---
+++ STLProject/Assets/Scripts/Logic/TimedPressurePlateDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPressurePlateDoor : MonoBehaviour {
+
+    public List<BooleanLogic> plates = new List<BooleanLogic>();
+
+    public Collider2D col;
+    public SpriteRenderer rend;
+
+    private void Start() {
+        foreach(BooleanLogic l in plates) {
+            l.onChange.AddListener(checkSolution);
+        }
+    }
+
+    void checkSolution() {
+        foreach (BooleanLogic l in plates) {
+            if (!l.getState()) {
+                return;
+            }
+        }
+        col.enabled = false;
+        rend.enabled = false;
+        this.enabled = false;
+    }
+
+}
