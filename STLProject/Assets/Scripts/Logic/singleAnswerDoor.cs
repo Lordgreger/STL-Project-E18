0e783475faf7287ff623bebb7c0c6bc41ec2c0e9
Mette Ohlsen <mette.tanggaard.ohlsen@gmail.com>
2018-11-01 18:22:46 +0100

    demoScene2 and new door sprites

---
+++ STLProject/Assets/Scripts/Logic/singleAnswerDoor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singleAnswerDoor : MonoBehaviour {
+
+    public Sprite open;
+    public Sprite closed;
+
+    public SpriteRenderer rend;
+
+    public Collider2D doorCollider;
+
+    public List<BooleanLogic> offLevers = new List<BooleanLogic>();
+    public BooleanLogic onLever;
+
+    private void Start()
+    {
+        foreach(BooleanLogic l in offLevers){
+            l.onChange.AddListener(checkSolution);
+        }
+        onLever.onChange.AddListener(checkSolution);
+    }
+
+    void checkSolution(){
+        if(onLever.getState()) {
+            foreach (BooleanLogic l in offLevers)
+            {
+                if(l.getState()){
+                    rend.sprite = closed;
+                    doorCollider.enabled = true;
+                    return;
+                }
+            }
+            rend.sprite = open;
+            doorCollider.enabled = false;
+        } else {
+            rend.sprite = closed;
+            doorCollider.enabled = true;
+        }
+    }
+
+}
