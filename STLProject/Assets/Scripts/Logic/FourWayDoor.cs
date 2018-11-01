a58ecde70c69572f4e58755ed83a840cb876f182
Natascha Grønlie <natgroenlie@outlook.dk>
2018-11-01 14:16:41 +0100

    Mit navn er Natascha og jeg har lavet nogle ændringer

---
+++ STLProject/Assets/Scripts/Logic/FourWayDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourWayDoor : MonoBehaviour {
+
+    public List<FourWaySimpel> symbols = new List<FourWaySimpel>();
+    public List<int> code = new List<int>();
+    public SpriteRenderer door;
+    public Collider2D doorCollider;
+
+
+    private void Start()
+	{
+        foreach (FourWaySimpel fws in symbols)
+        {
+            fws.onChange.AddListener(updateDoor);
+        }
+	}
+
+
+	private void updateDoor(){
+        for (int i = 0; i < symbols.Count; i++){
+            if (symbols[i].getState() != code[i]){
+                door.enabled = true;
+                doorCollider.enabled = true;
+                return;
+            }
+        }
+        door.enabled = false;
+        doorCollider.enabled = false;
+
+    }
+}
---
+++ STLProject/Assets/Scripts/Logic/FourWaySimpel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourWaySimpel : IntegerLogic {
+
+    public int startingState;
+    public Transform symbol;
+	
+
+    private void Start()
+	{
+        state = startingState;
+        updateVisuals();
+	}
+
+	private void updateVisuals()
+	{
+        symbol.eulerAngles = new Vector3(0, 0, state * 90);
+	}
+
+    public void onInteract()
+    {
+        if (state > 2) {
+            state = 0;
+            updateVisuals();
+            onChange.Invoke();
+        }
+        else{
+            state++;
+            updateVisuals();
+            onChange.Invoke();
+        }
+    }
+
+}
