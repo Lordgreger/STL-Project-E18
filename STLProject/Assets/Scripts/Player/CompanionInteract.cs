45fd1b9ac7abad6d29936d9ddc9abcf9151acd21
Greger <blacknedpsi@gmail.com>
2018-11-07 00:02:14 +0100

    Some code companions

--- STLProject/Assets/Scripts/Player/CompanionInteract.cs
+++ STLProject/Assets/Scripts/Player/CompanionInteract.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 
 public class CompanionInteract : MonoBehaviour {
 
+    // Companion 1
     public CompanionInterface c1;
-    public TextMeshProUGUI interactionText;
-    public Animator ani;
+    public TextMeshProUGUI c1InteractionText;
+    public Animator c1Ani;
+    public CompanionInteractable c1ActiveInteractable;
+
+    // Companion 2
+    public CompanionInterface c2;
+    public TextMeshProUGUI c2InteractionText;
+    public Animator c2Ani;
+    public CompanionInteractable c2ActiveInteractable;
 
     CompanionInteractable currentInteractable;
 
     private void Start() {
         currentInteractable = null;
-        interactionText.text = "";
-        c1.companionMovement.onStateChange.AddListener(updateInteractable);
+
+        c1InteractionText.text = "";
+        c1.companionMovement.onStateChange.AddListener(updateC1);
+        c1ActiveInteractable = null;
+
+        c2InteractionText.text = "";
+        c2.companionMovement.onStateChange.AddListener(updateC2);
+        c2ActiveInteractable = null;
     }
 
     private void Update() {
         if (currentInteractable != null) {
-            if (Input.GetKeyDown(KeyCode.Q)) {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
                 currentInteractable.interact(c1);
             }
         }
@@ -27,33 +41,38 @@ public class CompanionInteract : MonoBehaviour {
 
     public void setInteractable(CompanionInteractable interactable) {
         currentInteractable = interactable;
-        updateInteractable();
+        updateC1();
     }
 
     public void removeInteractable(CompanionInteractable interactable) {
         if (currentInteractable == interactable) {
             //Debug.Log("Got called!");
             currentInteractable = null;
-            ani.SetBool("Active", false);
+            c1Ani.SetBool("Active", false);
         }
     }
 
-    void updateInteractable() {
+    void updateC1() {
         if (currentInteractable != null) {
-            if (c1.companionMovement.isFollowing()) {
-                interactionText.text = "[Q] " + c1.companionName + " " + currentInteractable.interactionText;
-                ani.SetBool("Active", true);
+            if (c1.companionMovement.isFollowing() && ()) {
+                c1InteractionText.text = "[Q] " + c1.companionName + " " + currentInteractable.interactionText;
+                c1Ani.SetBool("Active", true);
             }
             else if (c1.companionMovement.isMoving()) {
-                ani.SetBool("Active", false);
+                c1Ani.SetBool("Active", false);
             }
             else {
-                interactionText.text = "[Q] " + c1.companionName + " follow me";
-                ani.SetBool("Active", true);
+                if (c1ActiveInteractable == currentInteractable) {
+                    c1InteractionText.text = "[Q] " + c1.companionName + " follow me";
+                    c1Ani.SetBool("Active", true);
+                }
+                c1InteractionText.text = "[Q] " + c1.companionName + " follow me";
+                c1Ani.SetBool("Active", true);
             }
         }
         else {
-            ani.SetBool("Active", false);
+            c1Ani.SetBool("Active", false);
+            c2Ani.SetBool("Active", false);
         }
     }
 
