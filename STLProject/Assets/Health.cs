3ac9866e2785d7cb5d2e8f9ad73d319e1c4ebb8b
Greger <blacknedpsi@gmail.com>
2018-11-14 23:07:27 +0100

    trap and health

--- STLProject/Assets/Health.cs
+++ STLProject/Assets/Health.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
-    
+
+    public float maxHealth;
+
+    public float health;
+
+
+    private void Start() {
+        health = maxHealth;
+    }
+
+    public void damage(float damage) {
+        health -= damage;
+    }
 }
---
+++ STLProject/Assets/Scripts/Traps/BaseFloorTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseFloorTrap : MonoBehaviour {
+
+    public float damage;
+    public float damageCooldown;
+    public Collider2D col;
+
+    bool active = true;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Companion") {
+            if (active) {
+                onTriggerTrap();
+                active = false;
+                StartCoroutine(resetCooldown());
+            }
+        }
+    }
+
+    IEnumerator resetCooldown() {
+        yield return new WaitForSeconds(damageCooldown);
+        active = true;
+    }
+
+    public virtual void onTriggerTrap() {
+        print("Trap Triggered");
+    }
+
+}
