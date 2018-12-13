c04012bcc3877041b8976625af9039ff213afa87
Greger <blacknedpsi@gmail.com>
2018-12-13 15:30:43 +0100

    Puzzles done

---
+++ STLProject/Assets/Scripts/Logic/Base/BooleanLogicCombiner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BooleanLogicCombiner : MonoBehaviour {
+
+    public List<BooleanLogic> bools = new List<BooleanLogic>();
+    public UnityEvent change;
+
+    private void Start() {
+        foreach (var b in bools) {
+            b.onChange.AddListener(onChange);
+        }
+    }
+
+    public void onChange() {
+        foreach (var b in bools) {
+            if (!b.getState()) {
+                return;
+            }
+        }
+        change.Invoke();
+    }
+}
---
+++ STLProject/Assets/Scripts/Logic/ToggleDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDoor : LogicDoor {
+
+    public bool state;
+
+    private void Start() {
+        setDoor(state);
+    }
+
+    public void toogle() {
+        state = !state;
+        setDoor(state);
+    } 
+
+    public void setState(bool b) {
+        state = b;
+        setDoor(state);
+    }
+}
