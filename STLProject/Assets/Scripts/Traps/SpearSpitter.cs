a01261111de215dfd89b95487e059e8cdbd7298c
Greger <blacknedpsi@gmail.com>
2018-12-14 06:40:53 +0100

    Layout done time to sleep some

--- STLProject/Assets/Scripts/Traps/SpearSpitter.cs
+++ STLProject/Assets/Scripts/Traps/SpearSpitter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpearSpitter : ArrowCreator {
+    private void Start() {
+        target = target + transform.position;
+    }
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.tag == "Player") {
